257ad93536788f39a6e4d8b6577f293e2ee1ba11
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-05 19:06:32 +0530

    [SARFARAAZ]-Prgram initialization

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day3_Employee_Wage
+{
+    public class EmployeeWage
+    {
+
+        public static void Main(string[] args)
+        {
+            //Welcome to Employee Wage computation problem
+
+          
+        }
+    }
+}
