29f524abbdee4df260ae064bdfb37da90ad192cc
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-05 19:09:33 +0530

    [SARFARAAZ]-Employee Attendance

--- Program.cs
+++ Program.cs
@@ -7,7 +7,20 @@
         {
             //Welcome to Employee Wage computation problem
 
-          
+
+            // UC1 Added
+            Random attendance = new Random();
+            int value = attendance.Next(0, 2);
+
+            if (value == 0)
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
+
         }
     }
 }
