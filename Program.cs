f54967147dedc7626137645a01d5591aee3891d4
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-06 14:53:45 +0530

    [SARFARAAZ]-UC5-Monthly wage program added

--- Program.cs
+++ Program.cs
@@ -9,20 +9,29 @@
 
 
             // UC1 Added
-            Random attendance = new Random();
-            int value = attendance.Next(0, 2);
+            Random attendanceCall = new Random();
+            int attendance = attendanceCall.Next(0, 2);
 
             //UC2 Added 
 
             int wagePerHour = 20;
             int fullDayHour = 8;
+            int workingDays = 20;
+            int monthlyWorkingHour = 100;
+
 
             //UC3 Added 
-            Random workinghr = new Random();
-            int workingHr = attendance.Next(0, 2);
+            int workingHr = attendanceCall.Next(0, 2);
             int partTime = 4;
 
-            if (value == 0)
+            //UC5
+            int wagePerDay = wagePerHour * fullDayHour;
+            int totalMonthlyWage = wagePerDay * workingDays;
+
+            Console.WriteLine("Total monthly wage is :{0}", totalMonthlyWage);
+
+
+          /*  if (attendance == 0)
             {
                 if (workingHr == 0)
                 {
@@ -35,13 +44,12 @@
                     Console.WriteLine("Employee todays wage is:" + partTime * wagePerHour);
                 }
                
-                           
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
                 Console.WriteLine("Employee todays wage is: 0");
-            }
+            }*/
 
         }
     }
