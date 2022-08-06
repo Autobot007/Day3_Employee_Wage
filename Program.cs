4642594414db6405bd1e8ca2ccb326235a7f560c
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-06 14:36:14 +0530

    [SARFARAAZ]-UC4 Switch Problem added

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@
 
 
             // UC1 Added
-            Random attendance = new Random();
-            int value = attendance.Next(0, 2);
+            Random attendanceCall = new Random();
+            int attendance = attendanceCall.Next(0, 2);
 
             //UC2 Added 
 
@@ -19,10 +19,39 @@
 
             //UC3 Added 
             Random workinghr = new Random();
-            int workingHr = attendance.Next(0, 2);
+            int workingHr = attendanceCall.Next(0, 2);
             int partTime = 4;
+            int workhour =0 ;
 
-            if (value == 0)
+
+            //UC4 Added
+
+            switch (attendance)
+            {
+                case 0:// if present this case will execute
+                    switch (workingHr)
+                    {
+                        case 0:// if present for half day this case will execute
+                            workhour = 4;
+                            Console.WriteLine("Employee is Present for half day");
+                            break;
+                        case 1:// if present for full day this case will execute
+                            workhour = 8;
+                            Console.WriteLine("Employee is Present for full day");
+                            break;
+                    }
+                    break;
+
+                case 1://if employee is absent this case will  execute.
+                    workhour = 0; 
+                    Console.WriteLine("Employee is absent");
+                    break;
+
+            }
+            Console.WriteLine("Employee's todays wage is:" + workhour * wagePerHour);
+            
+
+           /* if (value == 0)
             {
                 if (workingHr == 0)
                 {
@@ -42,7 +71,7 @@
                 Console.WriteLine("Employee is Absent");
                 Console.WriteLine("Employee todays wage is: 0");
             }
-
+           */
         }
     }
 }
